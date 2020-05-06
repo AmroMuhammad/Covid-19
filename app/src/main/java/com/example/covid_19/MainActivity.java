ccf542bf58a47d4608c984d7b1b25cd645ef99ea
Amr <amrmu94@gmail.com>
2020-05-06 23:46:00 +0200

    ViewPager added

--- app/src/main/java/com/example/covid_19/MainActivity.java
+++ app/src/main/java/com/example/covid_19/MainActivity.java
@@ -1,14 +1,61 @@
 package com.example.covid_19;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.fragment.app.Fragment;
+import androidx.viewpager.widget.ViewPager;
 
 import android.os.Bundle;
 
+import com.google.android.material.tabs.TabLayout;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class MainActivity extends AppCompatActivity {
+
+    private Toolbar toolbar;
+    private ViewPager viewPager;
+    private TabLayout tabLayout;
+    private WorldFragment worldFragment;
+    private CountriesFragment countriesFragment;
+    private  SavedFragment savedFragment;
+    private List<Fragment> fragments;
+    private List<String> fragmentTitles;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        toolbar = findViewById(R.id.toolbar);
+        viewPager = findViewById(R.id.viewPager);
+        tabLayout = findViewById(R.id.tabLayout);
+        setSupportActionBar(toolbar);
+
+        worldFragment = new WorldFragment();
+        countriesFragment = new CountriesFragment();
+        savedFragment = new SavedFragment();
+
+        tabLayout.setupWithViewPager(viewPager);
+        fragmentsinit();
+        fragmentTitlesinit();
+        ViewPagerAdaptor adaptor = new ViewPagerAdaptor(getSupportFragmentManager(),0,fragments,fragmentTitles);
+        viewPager.setAdapter(adaptor);
     }
+
+    private void fragmentTitlesinit() {
+        fragmentTitles = new ArrayList<>();
+        fragmentTitles.add("World");
+        fragmentTitles.add("Countries");
+        fragmentTitles.add("Saved");
+    }
+
+    private void fragmentsinit() {
+        fragments = new ArrayList<>();
+        fragments.add(worldFragment);
+        fragments.add(countriesFragment);
+        fragments.add(savedFragment);
+    }
+
+
 }
---
+++ app/src/main/java/com/example/covid_19/ViewPagerAdaptor.java
@@ -0,0 +1,42 @@
+package com.example.covid_19;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentPagerAdapter;
+import androidx.fragment.app.ListFragment;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ViewPagerAdaptor extends FragmentPagerAdapter {
+
+    private List<Fragment> mfragments;
+    private List<String> mfragmentTitles;
+
+    public ViewPagerAdaptor(@NonNull FragmentManager fm, int behavior, List<Fragment> fragments, List<String> fragmentTitles) {
+        super(fm, behavior);
+        mfragments = new ArrayList<>();
+        mfragmentTitles = new ArrayList<>();
+        mfragments = fragments;
+        mfragmentTitles = fragmentTitles;
+    }
+
+    @NonNull
+    @Override
+    public Fragment getItem(int position) {
+        return mfragments.get(position);
+    }
+
+    @Override
+    public int getCount() {
+        return mfragments.size();
+    }
+
+    @Nullable
+    @Override
+    public CharSequence getPageTitle(int position) {
+        return mfragmentTitles.get(position);
+    }
+}
