db143d967f4e3a77c8629e594b6fc3e91c2b2b70
Amr <amrmu94@gmail.com>
2020-05-18 03:32:06 +0200

    adjusting countries tab V2.3
    added Calender

--- app/src/main/java/com/example/covid_19/controller/activites/CountryDetails.java
+++ app/src/main/java/com/example/covid_19/controller/activites/CountryDetails.java
@@ -1,16 +1,26 @@
 package com.example.covid_19.controller.activites;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
+import android.app.DatePickerDialog;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.widget.CalendarView;
+import android.widget.DatePicker;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.example.covid_19.R;
 import com.squareup.picasso.Picasso;
 
-public class CountryDetails extends AppCompatActivity {
+import java.util.Calendar;
+
+public class CountryDetails extends AppCompatActivity implements DatePickerDialog.OnDateSetListener{
     TextView textView;
     ImageView imageView;
     Bundle bundle;
@@ -35,5 +45,49 @@ public class CountryDetails extends AppCompatActivity {
         else
             imageView.setImageResource(R.drawable.worldwide);
 
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.calender_menu,menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+            item.setOnMenuItemClickListener(new MenuItem.OnMenuItemClickListener() {
+                @Override
+                public boolean onMenuItemClick(MenuItem item) {
+                    Toast.makeText(CountryDetails.this, "a7aaaaa", Toast.LENGTH_SHORT).show();
+                   showDatePickerDialog();
+                    return false;
+                }
+            });
+        return super.onOptionsItemSelected(item);
+    }
+
+    private void showDatePickerDialog(){
+        DatePickerDialog datePickerDialog = new DatePickerDialog(
+                this,
+                this,
+                Calendar.getInstance().get(Calendar.YEAR),
+                Calendar.getInstance().get(Calendar.MONTH),
+                Calendar.getInstance().get(Calendar.DAY_OF_MONTH)
+        );
+        datePickerDialog.show();
+    }
+
+    @Override
+    public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
+        month = month+1;
+        if(dayOfMonth<10 && month<10)
+        Toast.makeText(this, year+"-0"+month+"-0"+dayOfMonth, Toast.LENGTH_SHORT).show();
+        else if(dayOfMonth<10)
+            Toast.makeText(this, year+"-"+month+"-0"+dayOfMonth, Toast.LENGTH_SHORT).show();
+        else if (month<10)
+            Toast.makeText(this, year+"-0"+month+"-"+dayOfMonth, Toast.LENGTH_SHORT).show();
+        else
+            Toast.makeText(this, year+"-"+month+"-"+dayOfMonth, Toast.LENGTH_SHORT).show();
     }
 }
